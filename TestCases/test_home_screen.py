43595f0c76de49de463444af97a97aafc1927ad9
Vipul <vipulsingh161@gmail.com>
2023-06-13 11:30:34 +0530

    Added logs to test_home_screen.py

--- TestCases/test_home_screen.py
+++ TestCases/test_home_screen.py
@@ -7,6 +7,7 @@ from TestCases.BaseTest import BaseTest
 class Test_Home_Screen(BaseTest):
 
     @pytest.mark.run(order=2)
+    @pytest.mark.usefixtures("log_on_failure")
     def test_home_screen(self):
         home = HomeScreen(self.driver)
         home.click_on_my_products()
